5fab1029e5c4ad9e99ce1a2c5b941097664491db
venthan002 <neethiventhan002@gmail.com>
2026-08-13 11:06:43 +0530

    Create Student.cs

---
+++ MVCExample/Models/Student.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCExample.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+    }
+}
